96231f935598bb5e5cc173dcff1eabea596c73af
RIFQI UMAMI <910222358@intranet.pajak.go.id>
2026-09-24 02:12:40 +0700

    fix format npwp for export csv pkm penagihan

--- app/Http/Controllers/PkmPenagihanController.php
+++ app/Http/Controllers/PkmPenagihanController.php
@@ -149,7 +149,7 @@ class PkmPenagihanController extends Controller
                 foreach ($query->cursor() as $row) {
                     fputcsv($file, [
                         $index++,
-                        isset($row->npwp15) ? "'{$row->npwp15}" : '',
+                        isset($row->npwp15) ? "{$row->npwp15}" : '',
                         $row->nama_wp,
                         $row->nip_jspn,
                         $row->nama_jspn,
